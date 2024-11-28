b6093ae5462937bff174b1abf484deb4acc0454b
LIJUNYU-94 <24aw0141@jec.ac.jp>
2024-11-28 10:38:01 +0900

    15

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import WeatherComponent from "./weatherComponent.jsx";
+import WeatherComponent from "./WeatherComponent.jsx";
 import Gallery from "./Profile.jsx";
 function App() {
   const [x, y] = useState(0);
